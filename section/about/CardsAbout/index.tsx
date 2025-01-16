7de54d33f5a3408eb84c9514936f41b8d90e7808
40radu <radurakotoarivelo@gmail.com>
2025-01-16 10:19:19 +0300

    feature : CardInfo

---
+++ section/about/CardsAbout/index.tsx
@@ -0,0 +1,26 @@
+import CardInfo from '@/components/common/card/CardInfo'
+import Container from '@/components/common/Container'
+import React from 'react'
+import "./style.scss"
+import classNames from 'classnames'
+import { dataCardInfo } from './data'
+
+
+function CardsAbout() {
+	return (
+		<Container className={classNames("cards-about")}>
+			{
+				dataCardInfo.map((dt, index) => (
+					<CardInfo
+						key={`card-info_${index}`}
+						image={dt.image}
+						title={dt.title}
+						firstDescription={dt.firstDescription}
+						secondDescription={dt.secondDescription} />
+				))
+			}
+		</Container>
+	)
+}
+
+export default CardsAbout
