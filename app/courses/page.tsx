81142d62f15b155df6c82bfe24e96351c1add2c0
40radu <radurakotoarivelo@gmail.com>
2025-01-24 11:32:48 +0300

    add static metadata

--- app/courses/page.tsx
+++ app/courses/page.tsx
@@ -1,5 +1,10 @@
 import HeroCommon from '@/components/common/section/HeroCommon'
+import { Metadata } from 'next'
 import React from 'react'
+
+export const metadata : Metadata = {
+	title : "Cours | Hianatra"
+}
 
 function Courses() {
 	return (
