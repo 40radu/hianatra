88bdb8e876262b852902f6c6cc40495bdb281818
santatra <mariosantatra@gmail.com>
2025-01-28 18:26:20 +0300

    implement internationalization for the home testimonial section

--- components/common/section/Testimonial/data.ts
+++ components/common/section/Testimonial/data.ts
@@ -1,22 +1,27 @@
-import { CardTestimonialProps } from "../../card/CardTestimonial"
+interface CardTestimonialData { 
+	image : string
+	starNumber : number
+	description : "descriOne" | "descriTwo" | "descriThree"
+	userName : string
+ }
 
-const cardTestimonialData: CardTestimonialProps[] = [
+const cardTestimonialData: CardTestimonialData[] = [
     {
         image: "/profil.png",
         starNumber: 5,
-        description: "Lorem Ipsum has been the industry's standard dumy text since the when took and scrambled to make type specimen book has survived. <br/> Lorem Ipsum has been the industry's standard dumy text since the when took and scrambled make.",
+        description: "descriOne",
         userName: "Tolotra.k"
     },
     {
         image: "/profil.png",
         starNumber: 4,
-        description: "Lorem Ipsum has been the industry's standard dumy text since the when took and scrambled to make type specimen book has survived. <br/> Lorem Ipsum has been the industry's standard dumy text since the when took and scrambled make.",
+        description: "descriTwo" ,
         userName: "Tolotra.l"
     },
     {
         image: "/profil.png",
         starNumber: 5,
-        description: "Lorem Ipsum has been the industry's standard dumy text since the when took and scrambled to make type specimen book has survived. <br/> Lorem Ipsum has been the industry's standard dumy text since the when took and scrambled make.",
+        description: "descriThree" ,
         userName: "Tolotra.m"
     },
 ]
--- locales/en/index.ts
+++ locales/en/index.ts
@@ -2,11 +2,13 @@ import header from "./common/header";
 import heroHome from "./home/heroHome";
 import homeAbout from "./home/homeAbout";
 import homeReason from "./home/homeReason";
+import homeTestimonial from "./home/homeTestimonial";
 
 export default {
   header,
   heroHome,
   homeReason,
   homeAbout,
+  homeTestimonial,
   viewMore: "View more"
 } as const
