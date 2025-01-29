6af1bdb931367dc1d06c4d1e8edb09b465216833
santatra <mariosantatra@gmail.com>
2025-01-29 14:09:52 +0300

    change text button

--- locales/fr/about/aboutWelcome.ts
+++ locales/fr/about/aboutWelcome.ts
@@ -7,5 +7,5 @@ export default {
   },
   description:
     "Lorem Ipsum has been the industr’s standard dummy text ever since unknown printer took galley type and scmbled make type specimen book. It has survived not only five centuries.",
-  button: "Get started",
+  button: "commencer le cour",
 };
