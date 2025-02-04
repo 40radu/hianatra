6fbab06bc3864c387858755c8a6030959ce40a20
santatra <mariosantatra@gmail.com>
2025-02-04 17:03:14 +0300

    delete bug

--- components/navigation/Button/index.tsx
+++ components/navigation/Button/index.tsx
@@ -3,26 +3,26 @@ import "./style.scss";
 import Icon, { IconType, IconVariant } from "../../Icon";
 
 interface ButtonProps {
-    label: string;
-    variant?: "blue" | "white" | "transparent";
-    icon?: keyof IconType;
-    iconVariant?: IconVariant;
-    className?: string;
-    type?: "submit" | "button";
-    onClick?: () => void
+	label: string;
+	variant?: "blue" | "white" | "transparent";
+	icon?: keyof IconType;
+	iconVariant?: IconVariant;
+	className?: string;
+	type?: "submit" | "button";
+	onClick?: () => void
 }
 
 export default function Button(props: ButtonProps) {
-    const { label, variant = "blue", icon, iconVariant = "blue", className = "", type = "button", onClick } = props;
-    return (
-        <button type={type} className={`btn btn--${variant} ${className}`} onClick={onClick}>`, `
-            {
-                icon &&
-                <span>
-                    <Icon name={icon} variant={iconVariant} />
-                </span>
-            }
-            <span>{label}</span>
-        </button>
-    );
+	const { label, variant = "blue", icon, iconVariant = "blue", className = "", type = "button", onClick } = props;
+	return (
+		<button type={type} className={`btn btn--${variant} ${className}`} onClick={onClick}>
+			{
+				icon &&
+				<span>
+					<Icon name={icon} variant={iconVariant} />
+				</span>
+			}
+			<span>{label}</span>
+		</button>
+	);
 }
